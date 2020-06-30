c2504589445ad0e8d8258485aa9fc06a938bded6
Nyannazz <kantemir.imam@gmail.com>
2020-06-30 22:17:48 +0200

    switching pc and second main screen section created

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-
+const siteHeader = document.querySelector('header');
+window.addEventListener('scroll', () => {
+    // once people are not at the top of the page make the header non transparent
+    window.scrollY ? siteHeader.classList.add('filled') : siteHeader.classList.remove('filled')
+})
